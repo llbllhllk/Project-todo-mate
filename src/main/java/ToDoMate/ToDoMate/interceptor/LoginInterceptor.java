d5a0b080cd6371a24e458fd3eeefef5586cdf221
DASOL KANG <dasol199@naver.com>
2022-01-23 16:35:47 +0900

    :wrench: update interceptor before login

--- src/main/java/ToDoMate/ToDoMate/interceptor/LoginInterceptor.java
+++ src/main/java/ToDoMate/ToDoMate/interceptor/LoginInterceptor.java
@@ -4,16 +4,18 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//
-//        if (request.getSession().getAttribute("id") == null){
-//            response.sendRedirect("/");
-//            return false;
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("member") == null){
+            response.sendRedirect("/");
+            return false;
+        }
+        return true;
+    }
 }
