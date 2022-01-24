b477b4d0ce5c85e16ec3366543095077e0408264
Kang Byeonghyeon <black1663@naver.com>
2022-01-24 23:38:30 +0900

    Fix: Find ID 사용자의 아이디, 인증번호 확인 관련 JS 수정

--- src/main/resources/static/javascripts/find-id.js
+++ src/main/resources/static/javascripts/find-id.js
@@ -3,6 +3,7 @@
 // Button
 const sendingCertificationBtn = document.querySelector('#sending-certification');
 const enteringCertificationBtn = document.querySelector('#confirm-certification');
+const closingModalBtn = document.querySelector('.modal__clos-btn');
 
 // Input
 const userEmail = document.querySelector('#user-email');
@@ -11,9 +12,13 @@ const userCertification = document.querySelector('#user-certification');
 // List
 const list = document.querySelector('.find-id__list.close');
 
+// Modal
+const modal = document.querySelector('.modal.close');
+const modalUserId = document.querySelector('.modal__user-id');
+
 // Alert
 const emptyEmail = document.querySelector('#empty-email');
-const worngEmail = document.querySelector('#wrong-email');
+const wrongEmail = document.querySelector('#wrong-email');
 const correctEmail = document.querySelector('#correct-email');
 const emptyCertification = document.querySelector('#empty-certification');
 const wrongCertification = document.querySelector('#wrong-certification');
@@ -23,6 +28,97 @@ const timeoutCertification = document.querySelector('#timeout-certification');
 // Timer
 const timer = document.querySelector('.find-id__timer');
 let interval;
+
+async function postUserEmail() {
+  try {
+    const emailUrl = '/validEmail';
+    const resValidEmail = await axios(emailUrl, {
+      "user-email": userEmail.value,
+    })
+    const validEmail = resValidEmail.data;
+    showEmailAlert(validEmail);
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
+
+async function postUserCertification() {
+  try {
+    const certificationUrl = '/validCertification'
+    const resUserId = await axios(certificationUrl, {
+      "user-certification": userCertification.value,  
+    })
+    const userId = resUserId.data;
+    showCertificationAlert(userId);
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
+
+async function postTimeoutCertification() {
+  try {
+    const timeoutCertificationUrl = '/timeoutCertification'
+    const resTimeoutCertification = await axios(timeoutCertificationUrl, {
+      timeout: true,
+    })
+    const validCertification = resValidCertification.data;
+    showCertificationAlert(validCertification);
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
+
+function showEmailAlert(validEmail) {
+  if(userEmail.value === "") {
+    // 이메일을 입력하지 않았을 경우
+    emptyEmail.classList.add('active');
+    wrongEmail.classList.remove('active');
+    correctEmail.classList.remove('active');
+  } 
+
+  if(validEmail === true) {
+    // 존재하는 이메일일 경우
+    emptyEmail.classList.remove('active');
+    wrongEmail.classList.remove('active');
+    correctEmail.classList.add('active')
+    // 인증번호 입력창 생성
+    list.classList.remove('close');
+    clearInterval(interval);
+    onTimer();
+    onClickEnteringCertificationBtn();
+
+  } else {
+    // 이메일이 틀렸을 경우 알림
+    emptyEmail.classList.remove('active');
+    wrongEmail.classList.add('active');
+    correctEmail.classList.remove('active')
+  }
+}
+
+function showCertificationAlert(id) {
+  // 인증번호를 입력하지 않았을 경우
+  if(userCertification.value === "") {
+    emptyCertification.classList.add('active');
+    wrongCertification.classList.remove('active');
+    correctCertification.classList.remove('active');
+  }
+  if(id === "") {
+    // 인증번호가 틀렸을 경우 틀렸다는 알림
+    emptyCertification.classList.remove('active');
+    wrongCertification.classList.add('active');
+    correctCertification.classList.remove('active'); 
+  } else {
+    // 인증번호가 맞을 경우 알림과 사용자의 아이디 modal로 출력
+    emptyCertification.classList.remove('active');
+    wrongCertification.classList.remove('active');
+    correctCertification.classList.add('active');
+    modal.classList.remove('close');
+    modalHandler(id);
+  }
+}
 
 function onTimer() {
   let minutes = 2;
@@ -42,53 +138,40 @@ function onTimer() {
     }
     if(minutes < 0) {
       clearInterval(interval);
-      // 제한시간 안에 인증번호 입력 못할 경우
     }
   }, 1000);
-}
-
-function showEmailAlert() {
-  if(userEmail.value === "") {
-    emptyEmail.classList.add('active');
-    worngEmail.classList.remove('active');
-    correctEmail.classList.remove('active')
-    // 이메일이 틀렸을 경우 알림
-  } else {
-    // 존재하는 이메일인 경우 알림과 인증번호 보내는 창 띄움
-    emptyEmail.classList.remove('active');
-    worngEmail.classList.remove('active');
-    correctEmail.classList.remove('active')
-    list.classList.remove('close');
-  }
+  // 제한시간이 초과됐을 경우
+  emptyCertification.classList.remove('active');
+  wrongCertification.classList.remoe('active');
+  correctCertification.classList.remove('active');
+  timeoutCertification.classList.add('active');
+  postTimeoutCertification();
 }
 
 function onClickSendingCertificationBtn() {
   sendingCertificationBtn.addEventListener('click', (e) => {
-    showEmailAlert();
-    clearInterval(interval);
-    onTimer();
+    postUserInfo();
   });
-}
-
-function showCertificationAlert () {
-  if(userCertification.value === "") {
-    emptyCertification.classList.add('active');
-    wrongCertification.classList.remove('active');
-    correctCertification.classList.remove('active');
-    // 인증번호가 틀렸을 경우 틀렸다는 알림
-    // 인증번호가 맞을 경우 알림과 사용자의 아이디 modal로 출력
-  }
 }
 
 function onClickEnteringCertificationBtn() {
   enteringCertificationBtn.addEventListener('click', (e) => {
-    showCertificationAlert();
+    postUserCertification();
   });
+}
+
+function modalHandler(id) {
+  // modal의 x버튼을 클릭하면 창 닫힘
+  closingModalBtn.addEventListener('click', () => {
+    modal.classList.add('close');
+  });
+  // modal에 해당 사용자의 아이디 출력
+  modalUserId.innerText = id;
 }
 
 function init() {
   onClickSendingCertificationBtn();
-  onClickEnteringCertificationBtn();
 }
 
 init();
+
